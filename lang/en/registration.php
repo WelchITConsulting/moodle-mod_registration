6680aa3f8edc293f0ea2248830b8b9b2fb4036f3
John Welch <jwelch@welchitconsulting.co.uk>
2015-02-08 20:54:20 +0000

    Correction of some of the strings for the interest_form

--- lang/en/registration.php
+++ lang/en/registration.php
@@ -68,7 +68,7 @@ $string['rejectsubject_default']        = 'Sorry, you have not been successful';
 $string['rejectsubject_help']           = 'This is the subject line for the email sent to those who have not been allocated a space at the event.';
 $string['starttime']                    = 'Event start';
 $string['submitbutton']                 = 'Register for event';
-$string['yesnofield']                   = 'Click if you would like to attend this event';
+$string['yesnofield']                   = 'Check this box if you would like to attend this event';
 
 
 //$string['']         = '';
