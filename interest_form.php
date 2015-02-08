f8ff075e7ddd6662e7f8d81db28a795d7065b5d6
John Welch <jwelch@welchitconsulting.co.uk>
2015-02-08 21:25:55 +0000

    Updated attributes for the other notes textbox

--- interest_form.php
+++ interest_form.php
@@ -34,7 +34,7 @@ class registration_interest_form extends moodleform
         $mform->setType('yesnofield', PARAM_BOOL);
         $mform->addRule('yesnofield', null, 'required', null, 'client');
 
-        $mform->addElement('textarea', 'notes', get_string('notes', 'registration'));
+        $mform->addElement('textarea', 'notes', get_string('notes', 'registration'), array('wrap' => 'virtual', 'rows' => '10', 'cols' => '50'));
         $mform->setType('notes', PARAM_RAW);
 
         $mform->addElement('hidden', 'id', 0);
